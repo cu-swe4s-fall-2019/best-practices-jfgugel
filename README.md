1cdac53471278d20e6dceb93eda462e6869d8714
Jack Gugel <jagu2118@colorado.edu>
2019-09-13 17:50:22 +0000

    Updated get_column_stats to coomply with style standards and best practices. Updated README to reflect this.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # best-practices
 
-Updates style.py to conform to style standards
+Updates style.py to conform to style standards.
+Updated get_column_stats.py to confrom to style standards and best practices.
--- get_column_stats.py
+++ get_column_stats.py
@@ -1,9 +1,24 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
+
+parser = argparse.ArgumentParser
+
+parser.add_argument('--file_name',
+                    type=str,
+                    help='Name of the file',
+                    required=True)
+
+parser.add_argument('--column_number',
+                    type=int,
+                    help='The name of the column',
+                    required=True)
+
+args = parser.parse_args()
 
 f = open(file_name, 'r')
+
 
 V = []
 
@@ -11,9 +26,48 @@ for l in f:
     A = [int(x) for x in l.split()]
     V.append(A[col_num])
 
+
+def get_array_mean(V):
+    '''
+    Compute the mean of the values in the column. Expects at least one value.
+
+    Parameters
+    -----------
+    V: List of intergers in the column
+
+    Returns
+    --------
+    mean: The mean of all the values in the column
+    '''
+
+
 mean = sum(V)/len(V)
+return mean
+if len(V) == 0
+# return an exit code of 1 for an empty column
+sys.exit(1)
+
+
+def get_array_mean(V):
+    '''
+    Compute the standard deviation of the values in the column.
+    Expects ate least one value
+
+    Parameters
+    ----------
+    V: List of integers in the column
+
+    Returns:
+    --------
+    stdev: The standard deviation of all the values in the column
+    '''
+
 
 stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+return stdev
+if len(V) == 0
+# return an exit code of 1 for an empty column
+sys.exit(1)
 
 print('mean:', mean)
 print('stdev:', stdev)
